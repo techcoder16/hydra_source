5a3b34503d4ba44342d92f184eecb5ce87b39bcd
furqan <88775196+Sheharyar21@users.noreply.github.com>
2023-07-21 00:55:43 +0500

    new

---
+++ Config.js
@@ -0,0 +1,43 @@
+const { ButtonStyle } = require("discord.js");
+module.exports = {
+    Token: "MTExNzE4Nzc1ODEwNTA0MzAxNQ.GhWNk3.j0dCgZVjsgW54OmTyFS-1GQKYCRdgWsDlQwh1k",// enter your token
+    Prefix: ".", // prefix here
+    Client: {
+        ID: "1117187758105043015", // client id
+    },
+    button: {
+        "grey": ButtonStyle.Secondary,
+        "blue": ButtonStyle.Primary,
+        "link": ButtonStyle.Link,
+        "red": ButtonStyle.Danger,
+        "green": ButtonStyle.Success
+    },
+    spotify: {
+        ID: "731ac5bf0603411f80ac446f5c02e290", // spotify client id
+        Secret: "cd16a34c385b4fa5915abd596fd4e480", // spotify client secret
+    },
+    MongoData: "mongodb+srv://Saavan:6969@saavan.mzc7ria.mongodb.net/Beta",// monngo db
+    EmbedColor: "#000000", // embed color
+    Owners: ["777538136782667796", "777538136782667796"], // owners in array
+    Nodes: [
+        {
+            name: 'krn.2d.gay', // Node Name
+            url: 'krn.2d.gay:443', //  Node Ip And : Port
+            auth: 'AWP)JQ$Gv9}dm.u', // Node Password
+            secure: true
+        }
+    ],
+    hooks: {
+        guildAdd: 'https://discord.com/api/webhooks/1031486168036016138/b2xBPrAtHpYJlWnAoF7nfk8p5IznorKmE_an8PsAwihXhHZh64iRpd75qM3jeI5jGa0M',
+        guildRemove: 'https://discord.com/api/webhooks/1031486168036016138/b2xBPrAtHpYJlWnAoF7nfk8p5IznorKmE_an8PsAwihXhHZh64iRpd75qM3jeI5jGa0M',
+    },
+    links: {
+        invite: 'https://discord.com/oauth2/authorize?client_id=1072189367247966219&scope=bot&permissions=831679622985',
+        bg: ("https://media.discordapp.net/attachments/1011709031687147550/1077159658281766912/maxresdefault_1.jpg"),
+        support: 'https://discord.gg/MSQeXpvJSV',
+        vote: 'https://discordbotlist.com/bots/qod-7134/upvote',
+        banner: 'https://media.discordapp.net/attachments/1011709031687147550/1077197657577107557/Picsart_23-02-20_17-28-14-257.jpg',
+        spotify: 'https://cdn.discordapp.com/attachments/1036701023990984724/1038352707720843345/782104564315717642.png',
+        soundcloud: 'https://cdn.discordapp.com/attachments/1036701023990984724/1038352707418849310/908400578776956978.png',
+    },
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+<center><img src="https://capsule-render.vercel.app/api?type=waving&color=gradient&height=200&section=header&text=Hydra&fontSize=80&fontAlignY=35&animation=twinkling&fontColor=gradient" /></center>
+<br>
+<br />
+
+[![Run on Repl.it](https://replit.com/badge/github/akabhijit/hydra)](https://repl.it/github/akabhijit/hydra)
+[![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/import/github/akabhijit/hydra)
+
+ ![Contributors](https://img.shields.io/github/contributors/akabhijit/hydra?color=dark-green) ![Forks](https://img.shields.io/github/forks/akabhijit/hydra?style=social) ![Stargazers](https://img.shields.io/github/stars/akabhijit/hydra?style=social)  ![License](https://img.shields.io/github/license/akabhijit/hydra)
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+
+  <h1 align="center">Hydra</h1>
+
+  <p align="center">
+    Clone Bot Of Hydra
+    <br />
+    <br />
+    <a href="https://discord.gg/UaMcC4PrYH">Support Server</a>
+    ·
+    <a href="https://github.com/akabhijit/hydra/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/akabhijit/hydra/issues">Request Feature</a>
+  </p>
+</p>
+
+## Developers
+
+@[AkAbhijit](https://github.com/akabhijit/)
+
+## 🎭 Features
+
+- ✅ Setup System
+- ✅ Music
+- ✅ 24/7
+- ✅ Dj
+- ✅ Custom prefix
+- ✅ Filters
+- ✅ Easy to use
+- ✅ And much more!
+
+## 💌 Support Server
+
+[![DiscordBanner](https://invidget.switchblade.xyz/UaMcC4PrYH)](https://discord.gg/UaMcC4PrYH)<br />
+[Support Server](https://discord.gg/UaMcC4PrYH) - Hydra's Support Server Invite
